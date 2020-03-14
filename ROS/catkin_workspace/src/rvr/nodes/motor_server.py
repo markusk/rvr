c182090cac2c5d2fb64794172be61ab8d6ea5e4d
Markus Knapp <github@direcs.de>
2020-03-14 22:11:43 +0100

    clean start with some ROS parts

--- ROS/catkin_workspace/src/rvr/nodes/motor_server.py
+++ ROS/catkin_workspace/src/rvr/nodes/motor_server.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-""" This is a service node (server) to control motors on the RVR.
+"""
+This is a service node (server) to control motors on the RVR.
 It controls the motors via [... tbd ...] on a Raspberry Pi.
 I expects the messsages "FORWARD, BACKWARD, LEFT, RIGHT, STOP".
 
@@ -10,7 +11,7 @@ Website: https://direcs.de
 """
 
 
-# name of the package(!).srv
+# name of the package.srv
 from rvr.srv import *
 import rospy
 
@@ -18,7 +19,7 @@ import rospy
 rospy.init_node('motor_server', anonymous=False)
 
 
-# for getting the hostname of the underlying system
+# getting the hostname of the underlying operating system
 import socket
 # showing hostname
 hostname = socket.gethostname()
@@ -28,156 +29,114 @@ else:
     rospy.logwarn("Running on host %s!", hostname)
 
 
-###############################
-###### motor stuff
-###############################
-# run some parts only on the real robot
+# RVR stuff
 if hostname == 'rvr':
-    rospy.loginfo("Setting up I2C...")
+    rospy.loginfo("Setting up RVR...")
+    
+    import os
+    import sys
+    # path to find the RVR lib from the SDK
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), './lib/')))
 
-    from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+    import asyncio
+    from sphero_sdk import SpheroRvrAsync
+    from sphero_sdk import SerialAsyncDal
+    from sphero_sdk import DriveFlagsBitmask
 
-    # create a default motor object, no changes to I2C address or frequency
-    mh = Adafruit_MotorHAT(addr=0x60)
 
-    # using motor 1 and 2 on RasPi hat
-    myMotor1 = mh.getMotor(1)
-    myMotor2 = mh.getMotor(2)
-    myMotor3 = mh.getMotor(3)
-    myMotor4 = mh.getMotor(4)
+    loop = asyncio.get_event_loop()
 
-    # recommended for auto-disabling motors on shutdown!
-    def turnOffMotors():
-        myMotor1.run(Adafruit_MotorHAT.RELEASE);
-        myMotor2.run(Adafruit_MotorHAT.RELEASE);
-        myMotor3.run(Adafruit_MotorHAT.RELEASE);
-        myMotor4.run(Adafruit_MotorHAT.RELEASE);
-
-    # turning off motors NOW - you never know...
-    turnOffMotors();
-
+    rvr = SpheroRvrAsync(
+        dal=SerialAsyncDal(
+            loop
+        )
+    )
 else:
-    rospy.logwarn("Skipping I2C setup. This is not the robot.")
+    rospy.logwarn("Skipping RVR setup. This is not the robot.")
 
 
 # define a clean node exit
 def my_exit():
-  rospy.loginfo("Shutting down motor service...")
-  # run some parts only on the real robot
-  if hostname == 'rvr':
-      turnOffMotors();
-  rospy.loginfo("...shutting down motor service complete.")
+    rospy.loginfo("Shutting down motor service...")
+    # run some parts only on the real robot
+    if hostname == 'rvr'
+        turnOffMotors();
+    rospy.loginfo("...shutting down motor service complete.")
 
-# call this method on node exit
+
+# call this method on ROS node exit
 rospy.on_shutdown(my_exit)
 
 
-# handle_motor is called with instances of MotorRequest and returns instances of MotorResponse
-# The request name comes directly from the .srv filename
-def handle_motor(req):
-    """ In this function all the work is done :) """
+async def main():
+    await rvr.wake()
 
-    # switch xxx to HIGH, if '1' was sent
-    if (req.direction == "FORWARD"): # and speed. returns result.
-        # drive
-        rospy.loginfo("Driving %s @ speed %s.", req.direction, req.speed)
-        if hostname == 'rvr':
-            # setting speed
-            myMotor1.setSpeed(req.speed)
-            myMotor2.setSpeed(req.speed)
-            myMotor3.setSpeed(req.speed)
-            myMotor4.setSpeed(req.speed)
-            # @todo: increase speed?
-            myMotor1.run(Adafruit_MotorHAT.FORWARD)
-            myMotor2.run(Adafruit_MotorHAT.BACKWARD)
-            myMotor3.run(Adafruit_MotorHAT.BACKWARD)
-            myMotor4.run(Adafruit_MotorHAT.FORWARD)
-    elif (req.direction == "BACKWARD"):
-        rospy.loginfo("Driving %s @ speed %s.", req.direction, req.speed)
-        if hostname == 'rvr':
-            # setting speed
-            myMotor1.setSpeed(req.speed)
-            myMotor2.setSpeed(req.speed)
-            myMotor3.setSpeed(req.speed)
-            myMotor4.setSpeed(req.speed)
-            # @todo: increase speed?
-            myMotor1.run(Adafruit_MotorHAT.BACKWARD)
-            myMotor2.run(Adafruit_MotorHAT.FORWARD)
-            myMotor3.run(Adafruit_MotorHAT.FORWARD)
-            myMotor4.run(Adafruit_MotorHAT.BACKWARD)
-    elif (req.direction == "LEFT"):
-        rospy.loginfo("Turning %s @ speed %s.", req.direction, req.speed)
-        if hostname == 'rvr':
-            # setting speed
-            myMotor1.setSpeed(req.speed)
-            myMotor2.setSpeed(req.speed)
-            myMotor3.setSpeed(req.speed)
-            myMotor4.setSpeed(req.speed)
-            # @todo: increase speed?
-            myMotor1.run(Adafruit_MotorHAT.BACKWARD)
-            myMotor2.run(Adafruit_MotorHAT.FORWARD)
-            myMotor3.run(Adafruit_MotorHAT.BACKWARD)
-            myMotor4.run(Adafruit_MotorHAT.FORWARD)
-    elif (req.direction == "RIGHT"):
-        rospy.loginfo("Turning %s @ speed %s.", req.direction, req.speed)
-        if hostname == 'rvr':
-            # setting speed
-            myMotor1.setSpeed(req.speed)
-            myMotor2.setSpeed(req.speed)
-            myMotor3.setSpeed(req.speed)
-            myMotor4.setSpeed(req.speed)
-            # @todo: increase speed?
-            myMotor1.run(Adafruit_MotorHAT.FORWARD)
-            myMotor2.run(Adafruit_MotorHAT.BACKWARD)
-            myMotor3.run(Adafruit_MotorHAT.FORWARD)
-            myMotor4.run(Adafruit_MotorHAT.BACKWARD)
-    elif (req.direction == "STOP"):
-        rospy.loginfo("Stopping.")
-        if hostname == 'rvr':
-            myMotor1.run(Adafruit_MotorHAT.RELEASE)
-            myMotor2.run(Adafruit_MotorHAT.RELEASE)
-            myMotor3.run(Adafruit_MotorHAT.RELEASE)
-            myMotor4.run(Adafruit_MotorHAT.RELEASE)
-        # @todo: also slow down?
-    else:
-      rospy.logerr("Direction '%s' not implemented.", req.direction)
+    # Give RVR time to wake up
+    await asyncio.sleep(2)
 
-    # The name of the 'xyzResponse' comes directly from the Xyz.srv filename!
-    # We return the speed as "okay"
-    return MotorResponse(req.speed)
+    await rvr.reset_yaw()
+
+    await rvr.drive_with_heading(
+        speed=128,  # Valid speed values are 0-255
+        heading=0,  # Valid heading values are 0-359
+        flags=DriveFlagsBitmask.none.value
+    )
+
+    # Delay to allow RVR to drive
+    await asyncio.sleep(1)
+
+    await rvr.drive_with_heading(
+        speed=128,  # Valid speed values are 0-255
+        heading=0,  # Valid heading values are 0-359
+        flags=DriveFlagsBitmask.drive_reverse.value
+    )
+
+    # Delay to allow RVR to drive
+    await asyncio.sleep(1)
+
+    await rvr.drive_with_heading(
+        speed=128,   # Valid speed values are 0-255
+        heading=90,  # Valid heading values are 0-359
+        flags=DriveFlagsBitmask.none.value
+    )
+
+    # Delay to allow RVR to drive
+    await asyncio.sleep(1)
+
+    await rvr.drive_with_heading(
+        speed=128,    # Valid speed values are 0-255
+        heading=270,  # Valid heading values are 0-359
+        flags=DriveFlagsBitmask.none.value
+    )
+
+    # Delay to allow RVR to drive
+    await asyncio.sleep(1)
+
+    await rvr.drive_with_heading(
+        speed=0,    # Valid heading values are 0-359
+        heading=0,  # Valid heading values are 0-359
+        flags=DriveFlagsBitmask.none.value
+    )
+
+    # Delay to allow RVR to drive
+    await asyncio.sleep(1)
+
+    await rvr.close()
 
 
-def motor_server():
-    # This declares a new service named 'motor with the Motor service type.
-    # All requests are passed to the 'handle_motor' function.
-    # 'handle_motor' is called with instances of MotorRequest and returns instances of MotorResponse
-    s = rospy.Service('motor', Motor, handle_motor)
-    rospy.loginfo("Ready to switch motors.")
+if __name__ == '__main__':
+    try:
+        loop.run_until_complete(
+            main()
+        )
 
-    # Keep our code from exiting until this service node is shutdown
-    rospy.spin()
+    except KeyboardInterrupt:
+        print('\nProgram terminated with keyboard interrupt.')
 
+        loop.run_until_complete(
+            rvr.close()
+        )
 
-if __name__ == "__main__":
-    motor_server()
-
-
-
-""" motor spped increase to be implemented...
-
-        rospy.loginfo("Speed up...")
-        for i in range(req.speed, 255):
-            myMotor1.setSpeed(i)
-            myMotor2.setSpeed(i)
-            myMotor3.setSpeed(i)
-            myMotor4.setSpeed(i)
-            time.sleep(0.01)
-
-        rospy.loginfo("Slow down...")
-        for i in range(255, req.speed, -1):
-            myMotor1.setSpeed(i)
-            myMotor2.setSpeed(i)
-            myMotor3.setSpeed(i)
-            myMotor4.setSpeed(i)
-            time.sleep(0.01)
-"""
+    finally:
+        if loop.is_running():
+            loop.close()
