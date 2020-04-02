7f6db5727003c4e4f18f5cf3e85ec6098f811014
Markus Knapp <github@direcs.de>
2020-04-03 00:11:45 +0200

    removed all hardware related stuff

--- ROS/catkin_workspace/src/rvr/nodes/motor_server_minibot.py
+++ ROS/catkin_workspace/src/rvr/nodes/motor_server_minibot.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-""" This is a service node (server) to control motors on the minibot
-(https://minibot.me). It controls the motors via I2C on a Raspberry Pi.
-I expects the messsages "FORWARD, BACKWARD, LEFT, RIGHT, STOP".
-
-Author:  Markus Knapp, 2019
-Website: https://direcs.de
-"""
-
 
 # name of the package(!).srv
 from rvr.srv import *
@@ -35,23 +27,10 @@ else:
 if hostname == 'rvrmate':
     rospy.loginfo("Setting up I2C...")
 
-    from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
-
-    # create a default motor object, no changes to I2C address or frequency
-    mh = Adafruit_MotorHAT(addr=0x60)
-
-    # using motor 1 and 2 on RasPi hat
-    myMotor1 = mh.getMotor(1)
-    myMotor2 = mh.getMotor(2)
-    myMotor3 = mh.getMotor(3)
-    myMotor4 = mh.getMotor(4)
 
     # recommended for auto-disabling motors on shutdown!
     def turnOffMotors():
-        myMotor1.run(Adafruit_MotorHAT.RELEASE);
-        myMotor2.run(Adafruit_MotorHAT.RELEASE);
-        myMotor3.run(Adafruit_MotorHAT.RELEASE);
-        myMotor4.run(Adafruit_MotorHAT.RELEASE);
+        rospy.loginfo("Motors turned off.")
 
     # turning off motors NOW - you never know...
     turnOffMotors();
@@ -83,61 +62,25 @@ def handle_motor(req):
         rospy.loginfo("Driving %s @ speed %s.", req.direction, req.speed)
         if hostname == 'rvrmate':
             # setting speed
-            myMotor1.setSpeed(req.speed)
-            myMotor2.setSpeed(req.speed)
-            myMotor3.setSpeed(req.speed)
-            myMotor4.setSpeed(req.speed)
             # @todo: increase speed?
-            myMotor1.run(Adafruit_MotorHAT.FORWARD)
-            myMotor2.run(Adafruit_MotorHAT.BACKWARD)
-            myMotor3.run(Adafruit_MotorHAT.BACKWARD)
-            myMotor4.run(Adafruit_MotorHAT.FORWARD)
     elif (req.direction == "BACKWARD"):
         rospy.loginfo("Driving %s @ speed %s.", req.direction, req.speed)
         if hostname == 'rvrmate':
             # setting speed
-            myMotor1.setSpeed(req.speed)
-            myMotor2.setSpeed(req.speed)
-            myMotor3.setSpeed(req.speed)
-            myMotor4.setSpeed(req.speed)
             # @todo: increase speed?
-            myMotor1.run(Adafruit_MotorHAT.BACKWARD)
-            myMotor2.run(Adafruit_MotorHAT.FORWARD)
-            myMotor3.run(Adafruit_MotorHAT.FORWARD)
-            myMotor4.run(Adafruit_MotorHAT.BACKWARD)
     elif (req.direction == "LEFT"):
         rospy.loginfo("Turning %s @ speed %s.", req.direction, req.speed)
         if hostname == 'rvrmate':
             # setting speed
-            myMotor1.setSpeed(req.speed)
-            myMotor2.setSpeed(req.speed)
-            myMotor3.setSpeed(req.speed)
-            myMotor4.setSpeed(req.speed)
             # @todo: increase speed?
-            myMotor1.run(Adafruit_MotorHAT.BACKWARD)
-            myMotor2.run(Adafruit_MotorHAT.FORWARD)
-            myMotor3.run(Adafruit_MotorHAT.BACKWARD)
-            myMotor4.run(Adafruit_MotorHAT.FORWARD)
     elif (req.direction == "RIGHT"):
         rospy.loginfo("Turning %s @ speed %s.", req.direction, req.speed)
         if hostname == 'rvrmate':
             # setting speed
-            myMotor1.setSpeed(req.speed)
-            myMotor2.setSpeed(req.speed)
-            myMotor3.setSpeed(req.speed)
-            myMotor4.setSpeed(req.speed)
             # @todo: increase speed?
-            myMotor1.run(Adafruit_MotorHAT.FORWARD)
-            myMotor2.run(Adafruit_MotorHAT.BACKWARD)
-            myMotor3.run(Adafruit_MotorHAT.FORWARD)
-            myMotor4.run(Adafruit_MotorHAT.BACKWARD)
     elif (req.direction == "STOP"):
         rospy.loginfo("Stopping.")
         if hostname == 'rvrmate':
-            myMotor1.run(Adafruit_MotorHAT.RELEASE)
-            myMotor2.run(Adafruit_MotorHAT.RELEASE)
-            myMotor3.run(Adafruit_MotorHAT.RELEASE)
-            myMotor4.run(Adafruit_MotorHAT.RELEASE)
         # @todo: also slow down?
     else:
       rospy.logerr("Direction '%s' not implemented.", req.direction)
@@ -160,24 +103,3 @@ def motor_server():
 
 if __name__ == "__main__":
     motor_server()
-
-
-
-""" motor spped increase to be implemented...
-
-        rospy.loginfo("Speed up...")
-        for i in range(req.speed, 255):
-            myMotor1.setSpeed(i)
-            myMotor2.setSpeed(i)
-            myMotor3.setSpeed(i)
-            myMotor4.setSpeed(i)
-            time.sleep(0.01)
-
-        rospy.loginfo("Slow down...")
-        for i in range(255, req.speed, -1):
-            myMotor1.setSpeed(i)
-            myMotor2.setSpeed(i)
-            myMotor3.setSpeed(i)
-            myMotor4.setSpeed(i)
-            time.sleep(0.01)
-"""
