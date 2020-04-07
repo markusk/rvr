c9ce25af22b25ee80ee86a45f181a1efa4bf70e6
Markus Knapp <ros@direcs.de>
2020-04-07 23:59:11 +0200

    updated entry point

--- ROS/catkin_workspace/src/ros2_test/setup.py
+++ ROS/catkin_workspace/src/ros2_test/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-		'talker = batteryPublisher.batteryPublisher:main',
+		'batteryPublisher = ros2test.batteryPublisher:main',
         ],
     },
 )
