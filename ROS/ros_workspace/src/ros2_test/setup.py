3385351a323212bbf1ee1389ee3a41c7976a7668
Markus Knapp <ros@direcs.de>
2020-04-12 18:27:45 +0200

    fixed path name in entry point

--- ROS/ros_workspace/src/ros2_test/setup.py
+++ ROS/ros_workspace/src/ros2_test/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-		'batteryPublisher = ros2test.batteryPublisher:main',
+		'batteryPublisher = ros2_test.batteryPublisher:main',
         ],
     },
 )
