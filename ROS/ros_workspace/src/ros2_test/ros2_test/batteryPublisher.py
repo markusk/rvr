bc6196b9f2830f68d57afdf30f4b7a1f1019c690
Markus Knapp <github@direcs.de>
2020-04-16 23:09:10 +0200

    removed timer staff

--- ROS/ros_workspace/src/ros2_test/ros2_test/batteryPublisher.py
+++ ROS/ros_workspace/src/ros2_test/ros2_test/batteryPublisher.py
@@ -37,24 +37,15 @@ async def main(args=None):
     publisher = node.create_publisher(String, 'topic', 10)
 
     msg = String()
-    i = 0
 
-    def timer_callback():
-        nonlocal i
-        msg.data = 'Hello RVR: %d' % i
-        i += 1
-        node.get_logger().info('Publishing: "%s"' % msg.data)
-        publisher.publish(msg)
+    # msg.data = 'Hello RVR: %d' % i
+    msg.data = 'Hello RVR!'
 
-    timer_period = 0.5  # seconds
-    timer = node.create_timer(timer_period, timer_callback)
+    node.get_logger().info('Publishing: "%s"' % msg.data)
+    publisher.publish(msg)
 
     rclpy.spin(node)
 
-    # Destroy the timer attached to the node explicitly
-    # (optional - otherwise it will be done automatically
-    # when the garbage collector destroys the node object)
-    node.destroy_timer(timer)
     node.destroy_node()
     rclpy.shutdown()
 
