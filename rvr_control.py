61cc32a7d12b8888a89c34a146b7113cba437384
Markus Knapp <github@direcs.de>
2020-05-02 23:09:02 +0200

    Adding Gamepad support...

--- rvr_control.py
+++ rvr_control.py
@@ -36,6 +36,10 @@ OK       = 1
 LOW      = 2
 CRITICAL = 3
 
+# Gamepad/Joystick device path
+fn = '/dev/input/js0'
+# True, when Gamepad connected and device opened
+gamepadConnected = False
 
 # OLED timing
 # wait time in seconds between different display information
@@ -97,6 +101,10 @@ from subprocess import call
 
 # for getting the hostname and IP of the underlying system
 import socket
+
+# For Gamepad / joystick
+import struct, array
+from fcntl import ioctl
 
 
 #-------------------
@@ -201,6 +209,89 @@ GPIO.setup(piezoPin,  GPIO.OUT)
 
 # LED OFF (low active!)
 GPIO.output(ledPin, GPIO.HIGH)
+
+
+# --------------
+# Gamepad stuff
+# --------------
+# We'll store the Gamepad states here.
+axis_states = {}
+button_states = {}
+
+# These constants were borrowed from linux/input.h
+axis_names = {
+    0x00 : 'x',
+    0x01 : 'y',
+    0x02 : 'z',
+    0x03 : 'rx',
+    0x04 : 'ry',
+    0x05 : 'rz',
+    0x06 : 'trottle',
+    0x07 : 'rudder',
+    0x08 : 'wheel',
+    0x09 : 'gas',
+    0x0a : 'brake',
+    0x10 : 'hat0x',
+    0x11 : 'hat0y',
+    0x12 : 'hat1x',
+    0x13 : 'hat1y',
+    0x14 : 'hat2x',
+    0x15 : 'hat2y',
+    0x16 : 'hat3x',
+    0x17 : 'hat3y',
+    0x18 : 'pressure',
+    0x19 : 'distance',
+    0x1a : 'tilt_x',
+    0x1b : 'tilt_y',
+    0x1c : 'tool_width',
+    0x20 : 'volume',
+    0x28 : 'misc',
+}
+
+button_names = {
+    0x120 : 'trigger',
+    0x121 : 'thumb',
+    0x122 : 'thumb2',
+    0x123 : 'top',
+    0x124 : 'top2',
+    0x125 : 'pinkie',
+    0x126 : 'base',
+    0x127 : 'base2',
+    0x128 : 'base3',
+    0x129 : 'base4',
+    0x12a : 'base5',
+    0x12b : 'base6',
+    0x12f : 'dead',
+    0x130 : 'a',
+    0x131 : 'b',
+    0x132 : 'c',
+    0x133 : 'x',
+    0x134 : 'y',
+    0x135 : 'z',
+    0x136 : 'tl',
+    0x137 : 'tr',
+    0x138 : 'tl2',
+    0x139 : 'tr2',
+    0x13a : 'select',
+    0x13b : 'start',
+    0x13c : 'mode',
+    0x13d : 'thumbl',
+    0x13e : 'thumbr',
+
+    0x220 : 'dpad_up',
+    0x221 : 'dpad_down',
+    0x222 : 'dpad_left',
+    0x223 : 'dpad_right',
+
+    # XBox 360 controller uses these codes.
+    0x2c0 : 'dpad_left',
+    0x2c1 : 'dpad_right',
+    0x2c2 : 'dpad_up',
+    0x2c3 : 'dpad_down',
+}
+
+axis_map = []
+button_map = []
 
 
 # piezo beep function
