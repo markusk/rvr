ad7f21db54b3c75ae7e865a37b2c7fe43b9fe42b
Markus Knapp <ros@direcs.de>
2020-05-02 11:45:28 +0200

    playing around with some soud...

--- melody.py
+++ melody.py
@@ -20,15 +20,17 @@ def buzz(noteFreq, duration):
 
 def play():
     t=0
+    dur=0.09
     # the notes have these frequencies: https://pages.mtu.edu/~suits/notefreqs.html
     # Bruder Jakob
     # notes=[262,294,330,262,262,294,330,262,330,349,392,330,349,392,392,440,392,349,330,262,392,440,392,349,330,262,262,196,262,262,196,262]
     # duration=[0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,0.5,1,0.5,0.5,1,0.25,0.25,0.25,0.25,0.5,0.5,0.25,0.25,0.25,0.25,0.5,0.5,0.5,0.5,1,0.5,0.5,1]
-    notes    = [392,   392,  392,  294,  392,  523, 262]
-    duration = [0.25, 0.25, 0.25, 0.25, 0.25, 0.25, 0.5]
+    notes    = [392,   392, 392,   1,  294,  392,  523,    1, 262]
+    duration = [dur,   dur, dur, 0.3,  dur,  dur,  dur,  0.5, dur]
     for n in notes:
         buzz(n, duration[t])
-        time.sleep(duration[t] *0.1)
+        # pause between each note
+        time.sleep(duration[t] *0.4)
         t+=1
 
 play()
