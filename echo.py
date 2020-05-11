c35a4cd6a297e8c9e6721ddb9645d377ed71d043
Markus Knapp <ros@direcs.de>
2020-05-11 22:44:47 +0200

    changed lib path added x attribute

--- echo.py
+++ echo.py
@@ -5,7 +5,7 @@
 import os
 import sys
 import time
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), './lib/')))
 
 from sphero_sdk import SpheroRvrObserver
 from sphero_sdk import SpheroRvrTargets
