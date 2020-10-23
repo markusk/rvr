20ad5026bf0accfe1bb40244edbea23579002da3
Markus Knapp <github@direcs.de>
2020-10-23 14:24:56 +0200

    fixed lib path

--- ROS/catkin_workspace/src/rvr/nodes/getBatteryState.py
+++ ROS/catkin_workspace/src/rvr/nodes/getBatteryState.py
@@ -3,7 +3,7 @@
 
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '/home/pi/develop/sphero-sdk-raspberrypi-python/')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../lib/')))
 
 import asyncio
 from sphero_sdk import SpheroRvrAsync
